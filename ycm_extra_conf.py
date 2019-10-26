68cf0f266f11b4cbd144042dfe89098fe205e318
suemi <suemi94@qq.com>
2019-10-26 08:28:08 +0000

    update ycmd to latest version

---
+++ ycm_extra_conf.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+flags = [
+'-Wall',
+'-Wextra',
+'-Werror',
+'-Wno-long-long',
+'-Wno-variadic-macros',
+'-fexceptions',
+'-DNDEBUG',
+'-std=c++17',
+'-x',
+'c++',
+'-I',
+'/usr/include',
+'-isystem',
+'/usr/lib/gcc/x86_64-linux-gnu/8/include',
+'-isystem',
+'/usr/include/x86_64-linux-gnu',
+'-isystem'
+'/usr/include/c++/8',
+'-isystem',
+'/usr/include/c++/8/bits'
+]
